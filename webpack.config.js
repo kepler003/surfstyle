7e2f71415fcf1a311e95e42998eb801233a818b9
kepler003 <b.pyzocha@gmail.com>
2020-11-04 19:10:37 +0100

    Add Qwigley, Roboto & Roboto Condensed fonts

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
       }
     ]
   },
