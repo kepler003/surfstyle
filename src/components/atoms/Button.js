007217d4aebdb7e5db61225eed448724b04b44fe
kepler003 <b.pyzocha@gmail.com>
2020-11-05 01:24:16 +0100

    Create Button component

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default ({ children, className, shape, bgColor, color, ...rest }) => {
+
+  let newClassName = `button ${ className }`;
+
+  // Set shape
+  switch (shape) {
+    case undefined: break;
+    case 'icon': newClassName += ' button--icon'; break;
+  };
+  
+  // Set background color
+  switch (bgColor) {
+    case undefined:     break;
+    case 'primary':     newClassName += ' button--primary'; break;
+    case 'transparent': newClassName += ' button--transparent'; break;
+  }
+  
+  // Set text color
+  switch (color) {
+    case undefined:   break;
+    case 'gray':      newClassName += ' button--color-gray'; break;
+    case 'white':     newClassName += ' button--color-white'; break;
+  }
+  
+  return (
+    <button className={ newClassName } type="button" { ...rest }>{ children }</button>
+  );
+};
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Logo from 'atoms/Logo';
+import Button from 'atoms/Button';
 
 export default () => (
   <header className="header">
     <Logo />
+    <Button shape="icon">H</Button>
   </header>
 );
