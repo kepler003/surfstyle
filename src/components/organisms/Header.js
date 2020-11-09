00c4fbc439d2e7b6034fd23fc171d3b9936f8ed7
kepler003 <b.pyzocha@gmail.com>
2020-11-09 13:18:31 +0100

    Header -> Add styling for all screen sizes

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Logo } from 'atoms/atoms';
 import { HeaderNav } from 'molecules/molecules';
 
-export default () => (
-  <header className="header">
-    <Logo />
-    <HeaderNav />
-  </header>
-);
+export default () => {
+  const [isActive, setIsActive] = useState(false);
+
+  
+  useEffect(() => {
+    window.addEventListener('scroll', toggleHeader);
+    
+    return () => {
+      window.removeEventListener('scroll', toggleHeader);
+    };
+  }, []);
+
+
+  const toggleHeader = () => setIsActive(() => window.scrollY > 0);
+
+  
+  return (
+    <header className={`header${isActive ? ' header--active' : ''}`}>
+      <Logo />
+      <HeaderNav />
+    </header>
+  );
+};
