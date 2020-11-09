80d416842a0879dae1ee177556e7b57534973e6c
kepler003 <b.pyzocha@gmail.com>
2020-11-09 18:08:50 +0100

    Organisms -> Places -> Add styling for all screen sizes

--- src/components/organisms/Places.js
+++ src/components/organisms/Places.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'atoms/Button';
+import image from 'images/surfboard.png';
 
 export default () => (
   <article className="places">
@@ -7,5 +8,6 @@ export default () => (
     <h2 className="places__heading">GUINCHO BEACH - CASCAIS - CALIFORNIA</h2>
     <p className="places__info">Surfing is such a great way to enjoy yourself in the ocean and we want ot teach you to surf whilst at the same time making sure you have a safe and super and super fun time! We have had years of experience teaching so many people to surf all over the world.</p>
     <Button className="places__btn" bgColor="primary" shape="wide" bold>Sign up</Button>
+    <img className="places__img" src={ image } alt="surfboard" />
   </article>
 );
