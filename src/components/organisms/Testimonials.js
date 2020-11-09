c90b8778eb163952144fae23c306b5e78cfea124
kepler003 <b.pyzocha@gmail.com>
2020-11-09 20:06:34 +0100

    Organisms -> Testimonials -> Add styling for all screen sizes

--- src/components/organisms/Testimonials.js
+++ src/components/organisms/Testimonials.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default () => (
-  <article>
-    <p>Testimonials</p>
+  <article className="testimonials">
+    <h3 className="testimonials__subheading">TESTIMONIALS</h3>
+    <h2 className="testimonials__heading">*HIGHLY RECOMMEND*</h2>
+    <p className="testimonials__info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra blandit vulputate porttitor lobortis amet turpis. Integer imperdiet nibh non adipiscing est dolor. Commodo felis ligula eget a. Sodales sed dignissim pretium in.</p>
   </article>
 );
