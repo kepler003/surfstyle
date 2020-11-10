274e7dfba092774e226d60b45a24494452714146
kepler003 <b.pyzocha@gmail.com>
2020-11-10 21:46:07 +0100

    Molecules -> Search -> Animate modal on close

--- src/components/molecules/Search.js
+++ src/components/molecules/Search.js
@@ -6,6 +6,7 @@ import { Button, Icon } from 'atoms/atoms';
 export default () => {
 
   const [isOpen, setIsOpen] = useState(false);
+  const [willClose, setWillClose] = useState(false);
 
 
   const openModal = () => {
@@ -14,9 +15,12 @@ export default () => {
 
   const closeModal = () => {
     setIsOpen(false);
+    setWillClose(true);
   };
 
   const autoFocus = input => input && input.focus();
+
+  const getParentSelector = () => document.querySelector('#root');
 
 
   const handleKeyUp = (e) => {
@@ -34,12 +38,13 @@ export default () => {
         <Icon icon="search" />
       </Button>
       <ReactModal
-        overlayClassName="search"
-        className=       "search__content"
-        parentSelector=  { () => document.querySelector('#root') }
+        overlayClassName={`search ${willClose ? 'search--close' : ''}`}
+        className=       {`search__content ${willClose ? 'search__content--close' : ''}}`}
+        parentSelector=  { getParentSelector }
         appElement=      { document.querySelector('#root') }
         isOpen=          { isOpen }
         onRequestClose=  { closeModal }
+        onAfterClose=    { () => setWillClose(false) }
         closeTimeoutMS=  { 250 }
       >
         <div className="search__box">
